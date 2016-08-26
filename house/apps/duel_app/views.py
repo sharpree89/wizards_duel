68818e123f9fbce55d71c56bc06bc2d886a518d0
Cindy Ngien <cindyngien@gmail.com>
2016-08-25 17:29:41 -0700

    added logic for duel

--- house/apps/duel_app/views.py
+++ house/apps/duel_app/views.py
@@ -6,40 +6,63 @@ def index(request):
 
 # Create your views here.
 def lockhart(request):
-    if "total" not in request.session:
-        request.session["total"] = 0
+    if "userhp" not in request.session:
+        request.session["userhp"] = 100
+
+    if "enemyhp" not in request.session:
+        request.session["enemyhp"] = 100
+
     if "log" not in request.session:
         request.session["log"] = []
     return render(request, "harry_app/lockhart.html")
 
 def process(request):
-    userhp = 100;
-    lockhartattack = random.randint(5, 10)
+    lockhartattack = random.randint(2, 5)
+    userhp = request.session["userhp"]
+    enemyhp = request.session["enemyhp"]
+
+    if userhp <= 0:
+        return redirect(reverse('duel:index'))
+    elif enemyhp <= 0:
+        return redirect(reverse('duel:index'))
+
     if request.method == "POST":
         if request.POST["spell"] == "flee":
             return redirect('duel:index')
 
         elif request.POST["spell"] == "heal":
-            healNum = random.randint(5, 10)
-            request.session["total"] += healNum
-            request.session["log"].append("You healed " + str(healNum) + " to your total HP!")
+            if request.session["userhp"] >= 95 and request.session["userhp"] < 100:
+                healNum = 1
+                request.session["userhp"] += healNum
+                userhp += healNum
+                request.session["log"].append("You healed " + str(healNum) + " to your total HP!")
+            elif request.session["userhp"] == 100:
+                healNum = 0
+                request.session["userhp"] += healNum
+                userhp += healNum
+                request.session["log"].append("Max health")
+            else:
+                healNum = random.randint(2, 5)
+                request.session["userhp"] += healNum
+                userhp += healNum
+                request.session["log"].append("You healed " + str(healNum) + " to your total HP!")
 
         elif request.POST["spell"] == "attack":
             attackNum = random.randint(2, 5)
-            request.session["total"] += attackNum
+            request.session["enemyhp"] -= attackNum
             request.session["log"].append("Expelliarmus hits for " + str(attackNum) + " damage!")
+            request.session["userhp"] -= lockhartattack
+            request.session["log"].append("Lockhart casts Obliviate on you for " + str(lockhartattack) + " damage!")
 
         elif request.POST["spell"] == "chance":
-            chanceNum = random.randint(-50, 50)
+            chanceNum = random.randint(-10, 10)
             if chanceNum < 0:
                 request.session["log"].append("Expulso blew up in your face! You lost " + str(chanceNum) + " hp!")
-                request.session["total"] += chanceNum
+                request.session["userhp"] += chanceNum
             else:
-                request.session["log"].append("Expulso was a success, " + str(chanceNum) + " damage done!")
-                request.session["total"] += chanceNum
-    request.session["log"].append("Lockhart casts Obliviate on you for " + str(lockhartattack) + " damage!")
-    if userhp == 0:
-        return redirect(reverse('duel:index'))
+                request.session["log"].append("Expulso was a success, " + " Lockhart takes " + str(chanceNum) + " damage")
+                request.session["enemyhp"] -= chanceNum
+
     return redirect(reverse('duel:lockhart'))
 
 
--- house/apps/house_app/views.py
+++ house/apps/house_app/views.py
@@ -1,28 +1,23 @@
 from django.shortcuts import render, redirect, HttpResponse
 from django.core.urlresolvers import reverse
-
+from ..hplogreg.models import User
 
 def index(request):
-    print '%%%%%%%%%% You are on the sorting page %%%%%%%%%%'
 
     return render(request, "house_app/all_houses.html")
 
 def gryffindor(request):
-    print '%%%%%%%%%% You are on the gryffindor page %%%%%%%%%%'
 
     return render(request, "house_app/gryffindor.html")
 
 def hufflepuff(request):
-    print '%%%%%%%%%% You are on the hufflepuff page %%%%%%%%%%'
 
     return render(request, "house_app/hufflepuff.html")
 
 def ravenclaw(request):
-    print '%%%%%%%%%% You are on the ravenclaw page %%%%%%%%%%'
 
     return render(request, "house_app/ravenclaw.html")
 
 def slytherin(request):
-    print '%%%%%%%%%% You are on the slytherin page %%%%%%%%%%'
 
     return render(request, "house_app/slytherin.html")
